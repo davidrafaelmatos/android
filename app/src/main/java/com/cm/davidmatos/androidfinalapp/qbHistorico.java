284924fd3d1c20c845b10fbab26091ee43bc45a5
davidrafaelmatos <david_matos_96@hotmail.com>
2018-06-21 16:54:49 +0100

    commit 210620181654

--- app/src/main/java/com/cm/davidmatos/androidfinalapp/qbHistorico.java
+++ app/src/main/java/com/cm/davidmatos/androidfinalapp/qbHistorico.java
@@ -2,6 +2,7 @@ package com.cm.davidmatos.androidfinalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class qbHistorico extends AppCompatActivity {
 
@@ -9,5 +10,25 @@ public class qbHistorico extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.qb_historico);
+
+        hideNavigationBar();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                );
     }
 }
--- app/src/main/java/com/cm/davidmatos/androidfinalapp/qbMain.java
+++ app/src/main/java/com/cm/davidmatos/androidfinalapp/qbMain.java
@@ -2,6 +2,7 @@ package com.cm.davidmatos.androidfinalapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ImageView;
 
 import com.jackandphantom.blurimage.BlurImage;
@@ -18,5 +19,24 @@ public class qbMain extends AppCompatActivity {
         blurView = (ImageView) findViewById(R.id.blurViewQB);
         BlurImage.with(getApplicationContext()).load(R.drawable.bg_boleia).intensity(10).Async(true).into(blurView);
 
+        hideNavigationBar();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideNavigationBar();
+    }
+
+    private void hideNavigationBar() {
+        this.getWindow().getDecorView()
+                .setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                                View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN |
+                                View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION |
+                                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                );
     }
 }
