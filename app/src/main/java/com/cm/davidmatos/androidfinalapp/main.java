657b1ae89e42491801aaba9aa99edc1b6f604adf
davidrafaelmatos <david_matos_96@hotmail.com>
2018-06-21 15:43:41 +0100

    commit 210620181543

--- app/src/main/java/com/cm/davidmatos/androidfinalapp/main.java
+++ app/src/main/java/com/cm/davidmatos/androidfinalapp/main.java
@@ -1,16 +1,40 @@
 package com.cm.davidmatos.androidfinalapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class main extends AppCompatActivity {
+
+    Button btnDouBoleia;
+    Button btnQueroBoleia;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         hideNavigationBar();
+
+        btnDouBoleia = (Button) findViewById(R.id.btnDouBoleia);
+        btnDouBoleia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main.this, dbMain.class);
+                startActivity(intent);
+            }
+        });
+
+        btnQueroBoleia = (Button) findViewById(R.id.btnQueroBoleia);
+        btnQueroBoleia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(main.this, qbMain.class);
+            }
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/com/cm/davidmatos/androidfinalapp/qbMain.java
@@ -0,0 +1,13 @@
+package com.cm.davidmatos.androidfinalapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class qbMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.qb_main);
+    }
+}
